107b46d4a1529f9ce9ac89f702849dbfee87ff97
Tanish Kamdar <kamdartanish0510@gmail.com>
2025-06-17 19:43:37 +0530

    Database Connection Setup with Mongoose successful

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,14 @@
 const express=require('express');
 const app=express();
-const PORT=3001;
+require('dotenv').config();
+const PORT=process.env.PORT || 3001;
+const mongoose=require('mongoose');
+const url=process.env.MONGO_URL;
+
+
+
 app.listen(PORT,()=>{
     console.log(`Listening to port ${PORT}`);
+    mongoose.connect(url);
+    console.log(`Database Connection Established.`);
 });
