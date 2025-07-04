3ff5bc9e18a07c352dead54ce9c7aadf533d4a82
Tanish Kamdar <kamdartanish0510@gmail.com>
2025-07-04 14:53:25 +0530

    Created the general context and its provider

---
+++ vite-dashboard/src/components/GeneralContext.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { createContext } from "react";
+
+const GeneralContext=createContext({
+    openBuyWindow : (uid)=>{},
+    closeBuyWindow : ()=>{}
+});
+
+export function GeneralContextProvider(props){
+    const [isBuyWindowOpen,setIsBuyWindowOpen]=useState(false);
+    const [selectedStockUID,setSelectedStockUID]=useState("");
+
+    function handleOpenBuyWindow(uid){
+        setIsBuyWindowOpen(true);
+        setSelectedStockUID(uid);
+    }
+
+    function handleCloseBuyWindow(){
+        setIsBuyWindowOpen(false);
+        setSelectedStockUID("");
+    }
+
+    return (<GeneralContext.Provider
+    value={
+        {
+           openBuyWindow : handleOpenBuyWindow,
+           closeBuyWindow : handleCloseBuyWindow
+        }
+    }
+    >
+        {props.children}
+        {isBuyWindowOpen && <BuyActionWindow uid={selectedStockUID} />}
+    </GeneralContext.Provider>);
+}
+
+export default GeneralContext;
