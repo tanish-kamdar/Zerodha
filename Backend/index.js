b0df3e20af8a18bb4fd3764747d93f4a56e9a0f5
Tanish Kamdar <kamdartanish0510@gmail.com>
2026-01-09 17:18:45 +0530

    Attempt to improve backend authentication and authorization architechture and logic, may not be final

--- Backend/index.js
+++ Backend/index.js
@@ -1,89 +1,114 @@
-const express=require('express');
-const app=express();
-require('dotenv').config();
-const PORT=process.env.PORT || 3001;
-const mongoose=require('mongoose');
-const url=process.env.MONGO_URL;
-const OrderModel = require('./models/OrderModel');
-const PositionModel = require('./models/PositionModel');
-const HoldingModel = require('./models/HoldingModel');
-const bodyParser=require('body-parser');
-const cors=require('cors');
-const userRoute=require('./routes/user');
-const cookieParser=require('cookie-parser');
-const morgan=require('morgan');
-const verifyToken = require('./middleware/verifyToken');
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const url = process.env.MONGO_URL;
+const OrderModel = require("./models/OrderModel");
+const PositionModel = require("./models/PositionModel");
+const HoldingModel = require("./models/HoldingModel");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const userRoute = require("./routes/user");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const requireAuth = require("./middleware/requireAuth");
 
-app.use(cors({
-        origin : 'http://localhost:5173',
-        credentials : true,
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(bodyParser.json());
 app.use(cookieParser(process.env.SECRET));
-app.use(morgan('dev'))
-app.get('/holdings',verifyToken, async (req,res)=>{
-       
-        let holdings=await HoldingModel.find();
-        res.send(holdings);
+app.use(morgan("dev"));
+app.get("/holdings", requireAuth, async (req, res) => {
+  let holdings = await HoldingModel.find();
+  console.log(
+    `Response Header before holdings sends a response :`,
+    res.getHeaders()
+  );
+  
+  res.json({
+    success: true,
+    holdings,
+  });
 });
 
-app.get('/positions',verifyToken,async (req,res)=>{
-        console.log(req.user);
-        let positions=await PositionModel.find();
-        res.send(positions);
+app.get("/positions", requireAuth, async (req, res) => {
+  console.log(req.user);
+  let positions = await PositionModel.find();
+  console.log(
+    `Response Header before positions sends a response :`,
+    res.getHeaders()
+  );
+  res.json({
+    success: true,
+    positions,
+  });
 });
-app.post('/order',verifyToken,async (req,res)=>{
-        let order=new OrderModel(req.body);
-        await order.save();
+app.post("/order", requireAuth, async (req, res) => {
+  let order = new OrderModel(req.body);
+  await order.save();
 
-        let holding=await HoldingModel.findOne({name:order.name});
-        
-        if(order.mode==="BUY"){
-                if(!holding){
-                        await HoldingModel.insertOne({
-                                name:order.name,
-                                qty: order.qty,
-                                avg: order.price,
-                                net : "+0.00%",
-                                day: "+0.00%"
-                        });
-                }else{
-                        let newQty=holding.qty+order.qty;
-                        
-                        await HoldingModel.updateOne({name:holding.name},{
-                                qty: newQty,
-                                avg: parseFloat(((holding.qty*holding.avg+order.qty*order.price)/(holding.qty+order.qty)).toFixed(2))
-                        });
-                }
+  let holding = await HoldingModel.findOne({ name: order.name });
 
-        }else if(order.mode==="SELL"){
-                if(!holding) return;
+  if (order.mode === "BUY") {
+    if (!holding) {
+      await HoldingModel.insertOne({
+        name: order.name,
+        qty: order.qty,
+        avg: order.price,
+        net: "+0.00%",
+        day: "+0.00%",
+      });
+    } else {
+      let newQty = holding.qty + order.qty;
 
-                let newQty=holding.qty-order.qty;
-
-                if(newQty===0){
-                        await HoldingModel.deleteOne({name:holding.name});
-                        return;
-                }else{
-                        await HoldingModel.updateOne({name:holding.name},{
-                                qty: newQty
-                        });
-                }
+      await HoldingModel.updateOne(
+        { name: holding.name },
+        {
+          qty: newQty,
+          avg: parseFloat(
+            (
+              (holding.qty * holding.avg + order.qty * order.price) /
+              (holding.qty + order.qty)
+            ).toFixed(2)
+          ),
         }
+      );
+    }
+  } else if (order.mode === "SELL") {
+    if (!holding) return;
+
+    let newQty = holding.qty - order.qty;
+
+    if (newQty === 0) {
+      await HoldingModel.deleteOne({ name: holding.name });
+      return;
+    } else {
+      await HoldingModel.updateOne(
+        { name: holding.name },
+        {
+          qty: newQty,
+        }
+      );
+    }
+  }
 });
 
-app.get('/holdings/:uid/qty',async (req,res)=>{
-        let uid=req.params.uid;
-        let qty=await HoldingModel.find({name : uid});
-        res.send(qty);
+app.get("/holdings/:uid/qty", async (req, res) => {
+  let uid = req.params.uid;
+  let qty = await HoldingModel.find({ name: uid });
+  res.send(qty);
 });
 
 //User Routes
-app.use("/user",userRoute);
+app.use("/user", userRoute);
 
-
-app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`);
-    mongoose.connect(url);
-    console.log(`Database Connection Established.`);
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+  mongoose.connect(url);
+  console.log(`Database Connection Established.`);
 });
--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,8 +1,10 @@
-const { Signup,Login } = require("../controllers/AuthController");
+const { Signup,Login, Logout, Refresh } = require("../controllers/AuthController");
 const app=require('express');
 const router=app.Router({mergeParams:true});
 
 router.post("/signup", Signup);
 router.post("/login", Login);
+router.get('/logout', Logout);
+router.get('/refresh', Refresh);
 
 module.exports=router;
