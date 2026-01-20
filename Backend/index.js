ec8e57d83b351b351515211cc77b8a05491e1509
Tanish Kamdar <kamdartanish0510@gmail.com>
2026-01-20 16:55:09 +0530

    Improved the Orders controller by adding log statements for debugging and sending a response which wasn't being sent previously

--- Backend/index.js
+++ Backend/index.js
@@ -49,13 +49,20 @@ app.get("/positions", requireAuth, async (req, res) => {
   });
 });
 app.post("/order", requireAuth, async (req, res) => {
+
+  if(!req.body) {
+    return res.status(400).json({message: "No order data provided"});
+  }
   let order = new OrderModel(req.body);
   await order.save();
+  console.log(`Order received : `, order);
 
   let holding = await HoldingModel.findOne({ name: order.name });
+  console.log(`Order mode : ${order.mode}`);
 
   if (order.mode === "BUY") {
     if (!holding) {
+      console.log(`No stocks found. Creating new holding for ${order.name}`);
       await HoldingModel.insertOne({
         name: order.name,
         qty: order.qty,
@@ -63,7 +70,9 @@ app.post("/order", requireAuth, async (req, res) => {
         net: "+0.00%",
         day: "+0.00%",
       });
+      console.log(`New holding created for ${order.name}`);
     } else {
+      console.log(`Existing holding found for ${order.name}`);
       let newQty = holding.qty + order.qty;
 
       await HoldingModel.updateOne(
@@ -78,14 +87,22 @@ app.post("/order", requireAuth, async (req, res) => {
           ),
         }
       );
+      console.log(`Holding updated for ${order.name}`);
     }
   } else if (order.mode === "SELL") {
-    if (!holding) return;
+    if (!holding) {
+      console.log(`No holdings found for ${order.name}. Cannot sell.`);
+
+      return res
+        .status(400)
+        .json({ message: `No holdings found for ${order.name}. Cannot sell.` });
+    }
 
     let newQty = holding.qty - order.qty;
 
     if (newQty === 0) {
       await HoldingModel.deleteOne({ name: holding.name });
+      console.log(`All stocks sold. Holding removed for ${order.name}`);
       return;
     } else {
       await HoldingModel.updateOne(
@@ -94,8 +111,10 @@ app.post("/order", requireAuth, async (req, res) => {
           qty: newQty,
         }
       );
+      console.log(`Holding updated for ${order.name}`);
     }
   }
+  res.status(201).json({ message: "Order placed successfully" });
 });
 
 app.get("/holdings/:uid/qty", async (req, res) => {
