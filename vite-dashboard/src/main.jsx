2736fcc2ab8d6cba30775cbd3d61e68b4f80ac0e
Tanish Kamdar <kamdartanish0510@gmail.com>
2026-01-20 16:10:24 +0530

    Strict Rendering disabled to prevent double rendering of components

--- vite-dashboard/src/main.jsx
+++ vite-dashboard/src/main.jsx
@@ -9,7 +9,7 @@ import { store } from "../redux/store";
 import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  // <StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
@@ -19,5 +19,5 @@ createRoot(document.getElementById("root")).render(
         </Routes>
       </BrowserRouter>
     </Provider>
-  </StrictMode>
+  // </StrictMode>
 );
